c466a441ad078aad20bcb9405b0b94ea7ae06756
Hunter-Kendall <hunter.kendall23@houhton.edu>
2023-03-14 15:47:27 -0400

    Changing our urls to the correct name

--- DoodleChamp/urls.py
+++ DoodleChamp/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('chat/', include('DoodleChamp_app.urls')),
+    path('DoodleChamp/', include('DoodleChamp_app.urls')),
     path('admin/', admin.site.urls),
 ]
--- DoodleChamp_app/routing.py
+++ DoodleChamp_app/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from DoodleChamp_app import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.DoodleChamp_appConsumer.as_asgi()),
+    re_path(r"ws/DoodleChamp/(?P<room_name>\w+)/$", consumers.DoodleChamp_appConsumer.as_asgi()),
 ]
