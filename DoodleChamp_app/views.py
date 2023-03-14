5d90621f75ebaa7ec27861519640d9f0767b3f7b
Gabrielmbl <gabrielmiranda7822@gmail.com>
2023-03-14 16:20:37 -0400

    Updated index.html and got it to join lobby.html

--- DoodleChamp_app/views.py
+++ DoodleChamp_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "chat/index.html")
+    return render(request, "game/index.html")
 
 def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name": room_name})
+    return render(request, "game/lobby.html", {"room_name": room_name})
