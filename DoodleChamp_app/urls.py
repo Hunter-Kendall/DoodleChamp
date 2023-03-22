c3101f00f7d4c8c6adb6738d8d6e0bc1a01b92a8
Hunter-Kendall <hunter.kendall23@houhton.edu>
2023-03-22 13:39:14 -0400

    connecting to lobby as host

--- DoodleChamp_app/urls.py
+++ DoodleChamp_app/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:room_name>/", views.room, name="room"),
+    path("<str:room_name>/", views.lobby, name="lobby"),
     path("<str:game_room_name>/", views.game_room, name="game_room")
 ]
--- DoodleChamp_app/views.py
+++ DoodleChamp_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from DoodleChamp_app.models import Lobby
 
 # Create your views here.
 
 def index(request):
     return render(request, "game/index.html")
 
-def room(request, room_name):
-    return render(request, "game/lobby.html", {"room_name": room_name})
+def lobby(request, room_name):
+    Lobby.objects.create(code = "room_id", host = request.POST["name"])
+    return render(request, "game/lobby.html", {"room_name": request.POST["name"]})#this is where we set room id
 
 def game_room(request, game_room_name):
     return render(request, "game/game.html", {"game_room_name": game_room_name})
