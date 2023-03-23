234db396867b4e408125f4404ef024e73c4768a0
Gabrielmbl <gabrielmiranda7822@gmail.com>
2023-03-23 15:34:17 -0400

    Added join function

--- DoodleChamp_app/urls.py
+++ DoodleChamp_app/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("Lobby/", views.lobby, name="lobby"),
+    path("Lobby/", views.create_lobby, name="lobby"),
     #path("<str:room_name>/", views.lobby, name="lobby"),
-    path("<str:game_room_name>/", views.game_room, name="game_room")
+    path("<str:game_room_name>/", views.game_room, name="game_room"),
+    path("Lobby/", views.join_lobby, name="join"),
 ]
--- DoodleChamp_app/views.py
+++ DoodleChamp_app/views.py
@@ -9,15 +9,15 @@ def index(request):
     return render(request, "game/index.html")
 
 def room_code():
-    code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4)) 
-    room_filter = Lobby.objects.filter(code = room_code)
-    if len(room_filter.values) == 0:
-        return code
+    random_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4)) 
+    room_filter = Lobby.objects.filter(code = random_code)
+    if len(room_filter.values()) == 0:
+        return random_code
     else:
         return room_code()
 
 # Make this be what gets called by create_lobby url. Drop room_name.
-def lobby(request):
+def create_lobby(request):
     # Randomly generate a room name.
     code = room_code()
     Lobby.objects.create(code = code, host = request.POST["name"])
@@ -31,4 +31,12 @@ def lobby(request):
 
 
 def game_room(request, game_room_name):
-    return render(request, "game/game.html", {"game_room_name": game_room_name})
+    # return render(request, "game/game.html", {"game_room_name": game_room_name})
+    pass 
+
+def join_lobby(request, room_code):
+    room_filter = Lobby.objects.filter(code = room_code)
+    if len(room_filter.values()) == 0:
+        return render(request, "game/lobby.html", {"room_name": room_code})
+    else:
+        pass
