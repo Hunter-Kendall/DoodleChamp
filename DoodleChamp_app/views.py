c6dbbd92e57445a61fc9ea5d8bce4db695b9d8ce
Gabrielmbl <gabrielmiranda7822@gmail.com>
2023-03-23 15:07:21 -0400

    Room code generator

--- DoodleChamp_app/views.py
+++ DoodleChamp_app/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
 from DoodleChamp_app.models import Lobby
+import string
+import random
 
 # Create your views here.
 
 def index(request):
     return render(request, "game/index.html")
 
+def room_code():
+    code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4)) 
+    room_filter = Lobby.objects.filter(code = room_code)
+    if len(room_filter.values) == 0:
+        return code
+    else:
+        return room_code()
+
 # Make this be what gets called by create_lobby url. Drop room_name.
-def lobby(request, room_name):
+def lobby(request):
     # Randomly generate a room name.
-    Lobby.objects.create(code = "room_id", host = request.POST["name"])
+    code = room_code()
+    Lobby.objects.create(code = code, host = request.POST["name"])
+
     # Pass in the room name (or room_id) and the user_name to be rendered on the page.
-    return render(request, "game/lobby.html", {"room_name": request.POST["name"]})#this is where we set room id
+    return render(request, "game/lobby.html", {"room_name": code})#this is where we set room id
     # In lobby.html, have the websocket connect and pass withit the room_id and user_name.
     # In the consumer, create a group for this game.
     # Coordinate communications between websockets and javascript.
