2d9ee26237d2f3cb7a71315088eab377d0347a9d
Gabrielmbl <gabrielmiranda7822@gmail.com>
2023-04-11 15:44:59 -0400

    Updated asgi.py

--- DoodleChamp/asgi.py
+++ DoodleChamp/asgi.py
@@ -14,11 +14,12 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+django_asgi_app = get_asgi_application()
+
 from DoodleChamp_app.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DoodleChamp.settings')
 
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
